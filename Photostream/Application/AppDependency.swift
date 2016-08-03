9749fd3b902477c9562e7335a677743d4a645393
Mounir Ybanez <rinuom91@gmail.com>
2016-08-03 15:03:55 +0800

    Added dependency and root wireframe classes.
    - The dependency class will handle setting up all the modules.
    - The root wireframe class will handle showing of the root view.

---
+++ Photostream/Application/AppDependency.swift
@@ -0,0 +1,13 @@
+//
+//  AppDependency.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 03/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class AppDependency: Object {
+
+}
---
+++ Photostream/Common/View/RootWireframe.swift
@@ -0,0 +1,16 @@
+//
+//  RootWireframe.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 03/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class RootWireframe: Object {
+
+    func showRoot(viewController: UIViewController, window: Window) {
+        
+    }
+}
