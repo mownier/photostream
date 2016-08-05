7d43afa6d4f535611d0f676ba3ca19634bb82b8f
Mounir Ybanez <rinuom91@gmail.com>
2016-08-05 16:08:16 +0800

    Implemented login via Firebase.

--- Photostream/Common/API/Firebase/LoginAPIFirebase.swift
+++ Photostream/Common/API/Firebase/LoginAPIFirebase.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseDatabase
 
 class LoginAPIFirebase: LoginServiceSource {
 
@@ -17,8 +18,26 @@ class LoginAPIFirebase: LoginServiceSource {
                 if let error = error {
                     callback(nil, error)
                 } else {
-                    let userModel = User()
-                    callback(userModel, error)
+                    guard let user = user else {
+                        callback(nil, NSError(domain: "LoginAPIFirebase", code: 1, userInfo: ["message": "FIRUser is nil."]))
+                        return
+                    }
+                    
+                    let id = user.uid
+                    let ref = FIRDatabase.database().reference()
+                    let path = "users/\(id)"
+                    ref.child(path).observeSingleEventOfType(.Value, withBlock: { (data) in
+                        guard let value = data.value else {
+                            callback(nil, NSError(domain: "LoginAPIFirebase", code: 2, userInfo: ["message": "FIRDataSnapshot is nil."]))
+                            return
+                        }
+                        var u = User()
+                        u.email = value["email"] as! String
+                        u.id = value["id"] as! String
+                        u.firstName = value["firstname"] as! String
+                        u.lastName = value["lastname"] as! String
+                        callback(u, nil)
+                    })
                 }
             }
         } else {
