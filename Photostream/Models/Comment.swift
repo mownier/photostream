5fc7b0374a92e1b90864ff17f7b04452145f2ffd
Mounir Ybanez <rinuom91@gmail.com>
2016-08-06 15:05:10 +0800

    Added Comment and Post models.

---
+++ Photostream/Models/Comment.swift
@@ -0,0 +1,17 @@
+//
+//  Comment.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 06/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import Foundation
+
+struct Comment  {
+    
+    var id: String!
+    var message: String!
+    var timestamp: Int64!
+    var user: User!
+}
---
+++ Photostream/Models/Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 06/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    
+    var id: String!
+    var user: User!
+    var image: String!
+    var timestamp: Int64!
+}
