6d9f2cf9ff4a8252f8a1227cbed9d651d688e59e
Mounir Ybanez <rinuom91@gmail.com>
2016-08-16 12:51:46 +0800

    Added view controller and storyboard.

---
+++ Photostream/Modules/News Feed/User Interface/View/NewsFeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsFeedViewController.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 16/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
