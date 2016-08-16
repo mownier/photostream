19c9f31ed0e280d7b85dd356e37da54dd5d4efaf
Mounir Ybanez <rinuom91@gmail.com>
2016-08-16 15:44:28 +0800

    Added views for news feed module.

---
+++ Photostream/Modules/News Feed/User Interface/View/NewsFeedHeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  NewsFeedHeaderView.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 16/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class NewsFeedHeaderView: UICollectionReusableView {
+        
+    @IBOutlet private weak var avatarImageView: UIImageView!
+    @IBOutlet private weak var displayNameLabel: UILabel!
+    
+    func setDisplayName(name: String) {
+        displayNameLabel.text = name
+    }
+    
+    func setAvatarUrl(url: String) {
+        avatarImageView.kf_setImageWithURL(NSURL(string: url))
+    }
+}
--- Photostream/Modules/News Feed/User Interface/View/NewsFeedViewController.swift
+++ Photostream/Modules/News Feed/User Interface/View/NewsFeedViewController.swift
@@ -7,12 +7,117 @@
 //
 
 import UIKit
+import MONUniformFlowLayout
 
 class NewsFeedViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: MONUniformFlowLayout!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addCustomTitleView()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        flowLayout.enableStickyHeader = true
+        collectionView.contentOffset = CGPointMake(0, 0)
+        collectionView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+        collectionView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 0, 0)
+    }
+    
+    private func addCustomTitleView() {
+        let titleView = UILabel(frame: CGRectZero)
+        let fontDesc = UIFontDescriptor(name: "Snell Roundhand", size: 24).fontDescriptorWithSymbolicTraits(.TraitBold)
+        titleView.font = UIFont(descriptor: fontDesc, size: fontDesc.pointSize)
+        titleView.text = "Photostream"
+        titleView.sizeToFit()
+        navigationItem.titleView = titleView
     }
 }
+
+extension NewsFeedViewController: MONUniformFlowLayoutDelegate {
+    
+    func collectionView(collectionView: UICollectionView!, layout: MONUniformFlowLayout!, itemHeightInSection section: Int) -> CGFloat {
+        return 100.0
+    }
+
+    func collectionView(collectionView: UICollectionView!, layout: MONUniformFlowLayout!, headerHeightInSection section: Int) -> CGFloat {
+        return 60
+    }
+}
+
+extension NewsFeedViewController: UICollectionViewDelegate {
+    
+}
+
+
+extension NewsFeedViewController: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("NewsFeedCell", forIndexPath: indexPath)
+        cell.backgroundColor = UIColor.greenColor()
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        
+        var reuseId: String!
+        if kind == UICollectionElementKindSectionHeader {
+            reuseId = "NewsFeedHeaderView"
+        } else {
+            reuseId = "NewsFeedFooterView"
+        }
+        
+        let reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: reuseId, forIndexPath: indexPath)
+        
+        if reuseId == "NewsFeedHeaderView" {
+            let headerView = reusableView as! NewsFeedHeaderView
+            headerView.setDisplayName("Wowowee")
+        }
+
+        return reusableView
+    }
+}
+
+extension UICollectionView {
+    
+    func headerHeightInSection(section: Int) -> CGFloat {
+        let del = delegate as! MONUniformFlowLayoutDelegate
+        let layout = collectionViewLayout as! MONUniformFlowLayout
+        return del.collectionView!(self, layout: layout, headerHeightInSection: section)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
