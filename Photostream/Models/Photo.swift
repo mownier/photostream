8f358be5e7bac96b616fef38f5b9bb7a6bb921e7
Mounir Ybanez <rinuom91@gmail.com>
2016-08-17 14:23:58 +0800

    Added photo model.

---
+++ Photostream/Models/Photo.swift
@@ -0,0 +1,21 @@
+//
+//  Photo.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 17/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import Foundation
+
+struct Photo {
+
+    var url: String!
+    var width: Int!
+    var height: Int!
+    
+    init() {
+        self.width = 0
+        self.height = 0
+    }
+}
