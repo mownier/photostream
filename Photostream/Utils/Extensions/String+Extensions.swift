622b65919a89b4d053d8113044c5e50ee28b83c8
Mounir Ybanez <rinuom91@gmail.com>
2016-08-17 16:16:44 +0800

    Added subscript extensions for String.

---
+++ Photostream/Utils/Extensions/String+Extensions.swift
@@ -0,0 +1,26 @@
+//
+//  String+Extensions.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 17/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+
+    subscript (i: Int) -> Character {
+        return self[self.startIndex.advancedBy(i)]
+    }
+    
+    subscript (i: Int) -> String {
+        return String(self[i] as Character)
+    }
+    
+    subscript (r: Range<Int>) -> String {
+        let start = startIndex.advancedBy(r.startIndex)
+        let end = start.advancedBy(r.endIndex - r.startIndex)
+        return self[Range(start ..< end)]
+    }
+}
