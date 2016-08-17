8d9f4e7fd08e1f910375a4e2b8a035afbeaf6f2c
Mounir Ybanez <rinuom91@gmail.com>
2016-08-17 16:18:23 +0800

    Added avatar related functions.
    - Added function for setting the avatar image view with placeholder.
    - Added function for creating a placeholder image for the avatar image view.

--- Photostream/Modules/News Feed/User Interface/View/NewsFeedHeaderView.swift
+++ Photostream/Modules/News Feed/User Interface/View/NewsFeedHeaderView.swift
@@ -13,12 +13,22 @@ class NewsFeedHeaderView: UICollectionReusableView {
 
     @IBOutlet private weak var avatarImageView: UIImageView!
     @IBOutlet private weak var displayNameLabel: UILabel!
-
+    
     func setDisplayName(name: String) {
         displayNameLabel.text = name
     }
 
-    func setAvatarUrl(url: String) {
-        avatarImageView.kf_setImageWithURL(NSURL(string: url))
+    func setAvatarUrl(url: String, placeholderImage: UIImage) {
+        avatarImageView.kf_setImageWithURL(NSURL(string: url), placeholderImage: placeholderImage, optionsInfo: nil, progressBlock: nil, completionHandler: nil)
+    }
+    
+    func createAvatarPlaceholderImage(initial: String) -> UIImage {
+        let width = avatarImageView.width
+        let height = avatarImageView.height
+        let frame = CGRectMake(0, 0, width, height)
+        let font = UIFont.systemFontOfSize(15)
+        let text: String = initial
+        let placeholderImage = UILabel.createPlaceholderImageWithFrame(frame, text: text, font: font)
+        return placeholderImage
     }
 }
