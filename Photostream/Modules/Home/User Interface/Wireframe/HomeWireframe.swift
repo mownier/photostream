360e9efe28f90fcc394300d5f836e1f29e24b9b4
Mounir Ybanez <rinuom91@gmail.com>
2016-08-17 11:51:24 +0800

    Added temporary setting of news feed module

--- Photostream/Modules/Home/User Interface/Wireframe/HomeWireframe.swift
+++ Photostream/Modules/Home/User Interface/Wireframe/HomeWireframe.swift
@@ -20,6 +20,13 @@ class HomeWireframe: AnyObject {
     func navigateHomeInterfaceFromWindow(window: UIWindow!) {
         let sb = UIStoryboard(name: "HomeModuleStoryboard", bundle: nil)
         homeViewController = sb.instantiateViewControllerWithIdentifier("HomeViewController") as! HomeViewController
+        
+        let nav = homeViewController.viewControllers?[0] as! UINavigationController
+        let vc = nav.viewControllers[0] as! NewsFeedViewController
+        let newsFeedWireframe = NewsFeedWireframe()
+        newsFeedWireframe.newsFeedPresenter.view = vc
+        vc.presenter = newsFeedWireframe.newsFeedPresenter
+        
         rootWireframe.showRootViewController(homeViewController, window: window)
     }
 }
