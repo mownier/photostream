284bba7f724cabd944c04be175eb1909fac7b82d
Mounir Ybanez <rinuom91@gmail.com>
2016-08-17 16:17:10 +0800

    Added generation of random color.

---
+++ Photostream/Utils/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extensions.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 17/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+    class func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+}
