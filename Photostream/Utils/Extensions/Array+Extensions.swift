df549b7acbfdef6ce657596d89bf0ca9524f3303
Mounir Ybanez <rinuom91@gmail.com>
2016-08-20 13:26:53 +0800

    Added index validation.

---
+++ Photostream/Utils/Extensions/Array+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Array+Extensions.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 20/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension Array {
+
+    func isValid(index: Int) -> Bool {
+        return index >= 0 && index < count
+    }
+}
