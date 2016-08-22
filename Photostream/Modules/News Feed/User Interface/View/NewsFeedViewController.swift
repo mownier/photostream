18b25c68040b789db429d46e977db2820f3c7ec5
Mounir Ybanez <rinuom91@gmail.com>
2016-08-22 16:13:11 +0800

    Refactored view controller.

--- Photostream/Modules/News Feed/User Interface/View/NewsFeedViewController.swift
+++ Photostream/Modules/News Feed/User Interface/View/NewsFeedViewController.swift
@@ -15,13 +15,15 @@ class NewsFeedViewController: UIViewController {
     @IBOutlet weak var flowLayout: MONUniformFlowLayout!
 
     var presenter: NewsFeedModuleInterface!
+    var displayItems = NewsFeedDisplayItemCollection()
+
     var sizingCell: NewsFeedCell!
     var cellHeights = [CGFloat]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        collectionView.registerNib(UINib(nibName: "NewsFeedCell", bundle: nil), forCellWithReuseIdentifier: "NewsFeedCell")
+        
+        NewsFeedCell.registerNibInto(collectionView)
 
         sizingCell = NewsFeedCell.createNew()
         addCustomTitleView()
@@ -46,31 +48,44 @@ class NewsFeedViewController: UIViewController {
         navigationItem.titleView = titleView
     }
 
-    private func configureCell(cell: NewsFeedCell, index: Int) {
-        let (post, user) = presenter.getPostAtIndex(UInt(index))
-        cell.setPhotoUrl(post.photo.url)
-        cell.setLikesCount(post.likesCount)
-        cell.setCommentsCount(post.commentsCount)
-        cell.setMessage(post.message, displayName: user.displayName)
-        cell.setElapsedTime(post.timestamp)
+    private func configureHeaderView(view: NewsFeedHeaderView, index: Int) {
+        if let item = displayItems[index] {
+            let displayName = item.headerItem.displayName
+            let avatarUrl = item.headerItem.avatarUrl
+            let placeholderImage = view.createAvatarPlaceholderImage(displayName[0])
+            view.setDisplayName(displayName)
+            view.setAvatarUrl(avatarUrl, placeholderImage: placeholderImage)
+        }
     }
     
-    private func computeExpectedCellHeight(cell: NewsFeedCell!, index: Int!) -> CGFloat {
+    private func configureCell(cell: NewsFeedCell, index: Int) {
+        if let item = displayItems[index] {
+            cell.setPhotoUrl(item.cellItem.photoUrl)
+            cell.setLikesCountText(item.cellItem.likes)
+            cell.setCommentsCountText(item.cellItem.comments)
+            cell.setMessage(item.cellItem.message, displayName: item.headerItem.displayName)
+            cell.setElapsedTime(item.cellItem.timestamp.timeAgoSinceNow())
+        }
+    }
+    
+    private func computeExpectedCellHeight(cell: NewsFeedCell!, index: Int!, width: CGFloat) -> CGFloat {
         configureCell(cell, index: index)
         cell.setNeedsLayout()
         cell.layoutIfNeeded()
         let size = cell.contentView.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
-        return size.height
+        let photoHeight = computeExpectedPhotoHeight(index, width: width)
+        return size.height + photoHeight
     }
 
     private func computeExpectedPhotoHeight(index: Int!, width: CGFloat) -> CGFloat {
-        let (post, _) = presenter.getPostAtIndex(UInt(index))
-
-        let photoWidth = post.photo.width
-        let photoHeight = post.photo.height
-
-        let ratio = width / CGFloat(photoWidth)
-        return  CGFloat(photoHeight) * ratio
+        if let item = displayItems[index] {
+            let photoWidth = item.cellItem.photoWidth
+            let photoHeight = item.cellItem.photoHeight
+            let ratio = width / CGFloat(photoWidth)
+            return  CGFloat(photoHeight) * ratio
+        }
+        
+        return -1
     }
 }
 
@@ -81,7 +96,15 @@ extension NewsFeedViewController: NewsFeedViewInterface {
     }
 
     func showEmptyView() {
-        // TODO: Show empty view
+
+    }
+    
+    func showError(error: NSError) {
+        
+    }
+    
+    func showItems(items: NewsFeedDisplayItemCollection) {
+        displayItems.appendContentsOf(items)
     }
 }
 
@@ -110,23 +133,16 @@ extension NewsFeedViewController: MONUniformFlowLayoutDelegate {
         if cellHeights.isValid(section) {
             return cellHeights[section]
         } else {
-            let cellHeight = computeExpectedCellHeight(sizingCell, index: section)
-            let photoHeight = computeExpectedPhotoHeight(section, width: collectionView.width)
-            let height = photoHeight + cellHeight
+            let height = computeExpectedCellHeight(sizingCell, index: section, width: collectionView.width)
             cellHeights.append(height)
             return height
         }
     }
 
     func collectionView(collectionView: UICollectionView!, layout: MONUniformFlowLayout!, headerHeightInSection section: Int) -> CGFloat {
-        return 54
+        return NewsFeedHeaderView.defaultHeight
     }
 }
-
-extension NewsFeedViewController: UICollectionViewDelegate {
-
-}
-
 
 extension NewsFeedViewController: UICollectionViewDataSource {
 
@@ -135,12 +151,11 @@ extension NewsFeedViewController: UICollectionViewDataSource {
     }
 
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return presenter.feedCount
+        return displayItems.count
     }
 
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("NewsFeedCell", forIndexPath: indexPath) as! NewsFeedCell
-
+        let cell = NewsFeedCell.dequeueFromCollectionView(collectionView, indexPath: indexPath)
         configureCell(cell, index: indexPath.section)
         cell.delegate = self
 
@@ -148,25 +163,16 @@ extension NewsFeedViewController: UICollectionViewDataSource {
     }
 
     func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
-
-        var reuseId: String!
         if kind == UICollectionElementKindSectionHeader {
-            reuseId = "NewsFeedHeaderView"
+            let headerView = NewsFeedHeaderView.dequeueFromCollectionView(collectionView, indexPath: indexPath)
+            configureHeaderView(headerView, index: indexPath.section)
+            
+            return headerView
         } else {
-            reuseId = "NewsFeedFooterView"
+            let reuseId = "NewsFeedFooterView"
+            let reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: reuseId, forIndexPath: indexPath)
+            
+            return reusableView
         }
-
-        let reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: reuseId, forIndexPath: indexPath)
-
-        if reuseId == "NewsFeedHeaderView" {
-            let headerView = reusableView as! NewsFeedHeaderView
-            let i = UInt(indexPath.section)
-            let (_, user) = presenter.getPostAtIndex(i)
-            let placeholderImage = headerView.createAvatarPlaceholderImage(user.firstName[0])
-            headerView.setDisplayName(user.fullName)
-            headerView.setAvatarUrl("", placeholderImage: placeholderImage)
-        }
-
-        return reusableView
     }
 }
