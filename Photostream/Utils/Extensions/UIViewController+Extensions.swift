6acef5f18341167f6d0ae23ef81e21ba1d4fbea5
Mounir Ybanez <rinuom91@gmail.com>
2016-08-29 16:22:44 +0800

    Added preloading of view.

---
+++ Photostream/Utils/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewController+Extensions.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 27/08/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func preloadView() {
+        if let _ = view {}
+    }
+}
