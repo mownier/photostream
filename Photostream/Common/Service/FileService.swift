da6142b789cfd3254993c00f13df7b7f43159b03
Mounir Ybanez <rinuom91@gmail.com>
2016-10-24 11:36:59 +0800

    Refactored file service.

--- Photostream/Common/Service/FileService.swift
+++ Photostream/Common/Service/FileService.swift
@@ -6,12 +6,41 @@
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-typealias FileServiceCallback = (String?, NSError?) -> Void
+protocol FileService {
 
-protocol FileService: class {
+    init(session: AuthSession)
+    
+    func uploadJPEGImage(data: FileServiceUploadData, width: Float, height: Float, callback: ((FileServiceResult) -> Void)?)
+}
 
-    init(session: AuthSession!)
-    func uploadImage(_ image: UIImage!, callback: FileServiceCallback!)
+struct FileServiceUploadData {
+    
+    var id: String = ""
+    var data: Data?
+}
+
+struct FileServiceResult {
+    
+    var uploadId: String?
+    var fileId: String?
+    var fileUrl: String?
+    var error: FileServiceError?
+}
+
+enum FileServiceError: Error {
+    
+    case authenticationNotFound(message: String)
+    case failedToUpload(message: String)
+    case noDataToUpload(message: String)
+    
+    var messsage: String {
+        switch self {
+        case .authenticationNotFound(let message),
+             .failedToUpload(let message),
+             .noDataToUpload(let message):
+            return message
+        }
+    }
 }
--- Photostream/Common/ServiceProvider/FileServiceProvider.swift
+++ Photostream/Common/ServiceProvider/FileServiceProvider.swift
@@ -12,54 +12,61 @@ import FirebaseDatabase
 
 class FileServiceProvider: FileService {
 
-    var session: AuthSession!
+    var session: AuthSession
 
-    required init(session: AuthSession!) {
+    required init(session: AuthSession) {
         self.session = session
     }
     
-    func uploadImage(_ image: UIImage!, callback: FileServiceCallback!) {
-        if let error = isOK() {
-            callback(nil, error)
-        } else {
-            let userId = session.user.id
-            let storageRef = FIRStorage.storage().reference()
-            let key = Int(Date.timeIntervalSinceReferenceDate * 1000)
-            let imagePath = "\(userId)/posts/\(key).jpg"
-            let imageData = UIImageJPEGRepresentation(image, 1.0)
-
-            let metadata = FIRStorageMetadata()
-            metadata.contentType = "image/jpeg"
-            storageRef.child(imagePath).put(imageData!, metadata: metadata, completion: { (metadata, error) in
-                if let error = error {
-                    callback(nil, error as NSError)
-                } else {
-                    if let imageUrl = metadata?.downloadURL()?.absoluteString {
-                        let width = image.size.width
-                        let height = image.size.height
-                        let databaseRef = FIRDatabase.database().reference()
-                        let key = databaseRef.child("photos").childByAutoId().key
-                        let path = "photos/\(key)"
-                        let data: [String: AnyObject] = ["id": key as AnyObject, "uid": userId as AnyObject, "url": imageUrl as AnyObject, "height": height as AnyObject, "width": width as AnyObject]
-                        databaseRef.child(path).setValue(data)
-                        callback(key, nil)
-                    } else {
-                        callback(nil, NSError(domain: "FileServiceProvider", code: 0, userInfo: ["message": "No download url."]))
-                    }
-                }
-            })
+    func uploadJPEGImage(data: FileServiceUploadData, width: Float, height: Float, callback: ((FileServiceResult) -> Void)?) {
+        var result = FileServiceResult()
+        result.uploadId = data.id
+        guard session.isValid else {
+            result.error = .authenticationNotFound(message: "Authentication not found.")
+            callback?(result)
+            return
         }
-    }
-
-    func isOK() -> NSError? {
-        if session.isValid {
-            return nil
-        } else {
-            if session.isValid {
-                return nil
-            } else {
-                return NSError(domain: "FileServiceProvider", code: 0, userInfo: ["message": "No authenticated user."])
+        
+        guard let imageData = data.data else {
+            result.error = .noDataToUpload(message: "No data to upload.")
+            callback?(result)
+            return
+        }
+        
+        let userId = session.user.id
+        let storageRef = FIRStorage.storage().reference()
+        let key = Int(Date.timeIntervalSinceReferenceDate * 1000)
+        let imagePath = "\(userId)/posts/\(key).jpg"
+        let metadata = FIRStorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        storageRef.child(imagePath).put(imageData, metadata: metadata, completion: { (metadata, error) in
+            guard error != nil else {
+                result.error = .failedToUpload(message: "Failed upload JPEG image.")
+                callback?(result)
+                return
             }
-        }
+            
+            guard let imageUrl = metadata?.downloadURL()?.absoluteString else {
+                result.error = .failedToUpload(message: "Image URL does not exist.")
+                callback?(result)
+                return
+            }
+            
+            let databaseRef = FIRDatabase.database().reference()
+            let key = databaseRef.child("photos").childByAutoId().key
+            let path = "photos/\(key)"
+            let data: [String: AnyObject] = [
+                "id": key as AnyObject,
+                "uid": userId as AnyObject,
+                "url": imageUrl as AnyObject,
+                "height": height as AnyObject,
+                "width": width as AnyObject ]
+            databaseRef.child(path).setValue(data)
+            
+            result.fileId = key
+            result.fileUrl = imageUrl
+            callback?(result)
+        })
     }
 }
