3a011d3d4b37cc4e5a92cbbbf17342b67f12f46d
Mounir Ybanez <rinuom91@gmail.com>
2016-11-01 11:55:20 +0800

    Refactored parsing of post cell item.

--- Photostream/Modules/News Feed/User Interface/Presenter/NewsFeedDisplayItemParser.swift
+++ Photostream/Modules/News Feed/User Interface/Presenter/NewsFeedDisplayItemParser.swift
@@ -10,32 +10,29 @@ import Foundation
 
 class NewsFeedDisplayItemParser: PostCellItemSerializer {
 
-    var post: NewsFeedPostData
-    var user: NewsFeedUserData
+    var post: NewsFeedPost
 
-    init(post: NewsFeedPostData, user: NewsFeedUserData) {
+    init(post: NewsFeedPost) {
         self.post = post
-        self.user = user
     }
 
     func serializeCellItem() -> PostCellItem {
         var item = PostCellItem()
 
-        item.postId = post.postId
-        item.userId = user.userId
+        item.postId = post.id
+        item.userId = post.userId
 
-        item.displayName = user.displayName
         item.isLiked = post.isLiked
         item.timestamp = Date(timeIntervalSince1970: post.timestamp)
         item.message = post.message
         item.photoUrl = post.photoUrl
         item.photoWidth = Float(post.photoWidth)
         item.photoHeight = Float(post.photoHeight)
-        item.commentsCount = Int(post.commentsCount)
-        item.likesCount = Int(post.likesCount)
+        item.commentsCount = Int(post.comments)
+        item.likesCount = Int(post.likes)
 
-        item.avatarUrl = user.avatarUrl
-        item.displayName = user.displayName
+        item.avatarUrl = post.avatarUrl
+        item.displayName = post.displayName
 
         return item
     }
