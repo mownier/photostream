e1d8fe59bc887c2eea414774542b38c0706e614e
Mounir Ybanez <rinuom91@gmail.com>
2016-11-10 15:56:30 +0800

    Added setting of camera, filter, and preview.

--- Photostream/UI/Photo Capture/PhotoCaptureViewController.swift
+++ Photostream/UI/Photo Capture/PhotoCaptureViewController.swift
@@ -15,10 +15,27 @@ class PhotoCaptureViewController: UIViewController {
     @IBOutlet weak var controlContentView: UIView!
     @IBOutlet weak var captureButton: UIButton!
     
+    lazy var camera = GPUImageStillCamera(sessionPreset: AVCaptureSessionPreset640x480, cameraPosition: .back)!
+    lazy var filter = GPUImageBrightnessFilter()
+    
+    var presenter: PhotoCaptureModuleInterface!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCamera()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        startCamera()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        stopCamera()
+        
+        super.viewWillDisappear(animated)
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -26,6 +43,57 @@ class PhotoCaptureViewController: UIViewController {
     }
     
     @IBAction func didTapCapture() {
+        // TODO: Implement image capture
+        presenter.capture()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func setupCamera() {
+        guard isCameraAvailable else {
+            return
+        }
+        
+        camera.outputImageOrientation = .portrait
+        preview.fillMode = kGPUImageFillModePreserveAspectRatioAndFill
+        filter.addTarget(preview)
+        camera.addTarget(filter)
+    }
+    
+    func startCamera() {
+        guard isCameraAvailable else {
+            return
+        }
+        
+        camera.startCapture()
+    }
+    
+    func stopCamera() {
+        guard isCameraAvailable else {
+            return
+        }
+        
+        camera.stopCapture()
+    }
+}
 
+extension PhotoCaptureViewController: PhotoCaptureViewInterface {
+    
+    var controller: UIViewController? {
+        return self
+    }
+    
+    var isCameraAvailable: Bool {
+        return UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    func capturedImage(with result: @escaping (UIImage?) -> Void) {
+        guard isCameraAvailable else {
+            result(nil)
+            return
+        }
+        
+        camera.capturePhotoAsImageProcessedUp(toFilter: filter) { (image, error) in
+            result(image)
+        }
     }
 }
