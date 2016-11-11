0a6bfde5ef2c264c7db76073585362b6aa5ef042
Mounir Ybanez <rinuom91@gmail.com>
2016-11-11 13:10:38 +0800

    Transferred the camera-related functions to module interface.

--- Photostream/Modules/Photo Capture/Presenter/PhotoCaptureModuleInterface.swift
+++ Photostream/Modules/Photo Capture/Presenter/PhotoCaptureModuleInterface.swift
@@ -7,9 +7,27 @@
 //
 
 import UIKit
+import GPUImage
 
 protocol PhotoCaptureModuleInterface: class {
 
+    var isCameraAvailable: Bool { get }
+    
     func capture()
     func cancel()
+    
+    func setupCamera(with preview: GPUImageView, cameraPosition: AVCaptureDevicePosition, preset: String, outputOrientation: UIInterfaceOrientation)
+    func startCamera()
+    func stopCamera()
+}
+
+extension PhotoCaptureModuleInterface {
+    
+    func setupBackCamera(with preview: GPUImageView, preset: String = AVCaptureSessionPreset640x480, outputOrientation: UIInterfaceOrientation = .portrait) {
+        setupCamera(with: preview, cameraPosition: .back, preset: preset, outputOrientation: outputOrientation)
+    }
+
+    func setupFrontCamera(with preview: GPUImageView, preset: String = AVCaptureSessionPreset640x480, outputOrientation: UIInterfaceOrientation = .portrait) {
+        setupCamera(with: preview, cameraPosition: .front, preset: preset, outputOrientation: outputOrientation)
+    }
 }
