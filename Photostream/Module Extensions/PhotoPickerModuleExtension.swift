e01f514108dddb36bd552056cfbf3d7b94e2cf8b
Mounir Ybanez <rinuom91@gmail.com>
2016-11-11 15:45:05 +0800

    Added module extension.

---
+++ Photostream/Module Extensions/PhotoPickerModuleExtension.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoPickerModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 11/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension PhotoPickerWireframe {
+    
+    class func createViewController() -> PhotoPickerViewController {
+        let sb = UIStoryboard(name: "PhotoPickerModuleStoryboard", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: "PhotoPickerViewController")
+        return vc as! PhotoPickerViewController
+    }
+}
