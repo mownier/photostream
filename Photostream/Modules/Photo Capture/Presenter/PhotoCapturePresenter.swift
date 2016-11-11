080dc5809acad46e2cd189edd686a95bfc1922ff
Mounir Ybanez <rinuom91@gmail.com>
2016-11-11 13:11:40 +0800

    Implemented module interface.

--- Photostream/Modules/Photo Capture/Presenter/PhotoCapturePresenter.swift
+++ Photostream/Modules/Photo Capture/Presenter/PhotoCapturePresenter.swift
@@ -7,23 +7,63 @@
 //
 
 import UIKit
+import GPUImage
 
 class PhotoCapturePresenter: PhotoCapturePresenterInterface {
 
     weak var moduleDelegate: PhotoCaptureModuleDelegate?
     weak var view: PhotoCaptureViewInterface!
     var wireframe: PhotoCaptureWireframeInterface!
+    var camera: GPUImageStillCamera?
+    var filter: GPUImageFilter?
 }
 
 extension PhotoCapturePresenter: PhotoCaptureModuleInterface {
     
+    var isCameraAvailable: Bool {
+        return UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
     func capture() {
-        view.capturedImage { (image) in
-            self.moduleDelegate?.photoCaptureDidFinish(with: image)
+        guard isCameraAvailable else {
+            return
         }
+        
+        camera?.capturePhotoAsImageProcessedUp(toFilter: filter, withCompletionHandler: { (image, error) in
+            self.moduleDelegate?.photoCaptureDidFinish(with: image)
+        })
     }
     
     func cancel() {
         moduleDelegate?.photoCaptureDidCanel()
+    }
+    
+    func setupCamera(with preview: GPUImageView, cameraPosition: AVCaptureDevicePosition = .back, preset: String = AVCaptureSessionPreset640x480, outputOrientation: UIInterfaceOrientation = .portrait) {
+        guard isCameraAvailable else {
+            return
+        }
+        
+        camera = GPUImageStillCamera(sessionPreset: preset, cameraPosition: cameraPosition)
+        camera?.outputImageOrientation = outputOrientation
+        
+        filter = GPUImageBrightnessFilter()
+        filter?.addTarget(preview)
+        camera?.addTarget(filter)
+    }
+    
+    func startCamera() {
+        guard isCameraAvailable  else {
+            return
+        }
+        
+        camera?.startCapture()
+    }
+    
+    func stopCamera() {
+        guard isCameraAvailable else {
+            return
+        }
+        
+        camera?.stopCapture()
     }
 }
