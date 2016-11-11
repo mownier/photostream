6916b2da5ceb0f71067697ef6802e1cbe0dcd7d0
Mounir Ybanez <rinuom91@gmail.com>
2016-11-11 14:49:29 +0800

    Added assembly of photo picker module.

--- Photostream/Modules/Photo Picker/Wireframe/PhotoPickerWireframe.swift
+++ Photostream/Modules/Photo Picker/Wireframe/PhotoPickerWireframe.swift
@@ -14,6 +14,16 @@ class PhotoPickerWireframe: PhotoPickerWireframeInterface {
     
     required init(root: RootWireframeInterface?, delegate: PhotoPickerModuleDelegate?, view: PhotoPickerViewInterface) {
         self.root = root
+        
+        let service = AssetServiceProvider()
+        let interactor = PhotoPickerInteractor(service: service)
+        let presenter = PhotoPickerPresenter()
+        
+        interactor.output = presenter
+        view.presenter = presenter
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.wireframe = self
     }
     
     func attachRoot(with controller: UIViewController, in window: UIWindow) {
