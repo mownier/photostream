5334393167943fcbb8f0b9e02f116ba340683b1c
Mounir Ybanez <rinuom91@gmail.com>
2016-11-11 15:54:48 +0800

    Added photo picker delegate.

---
+++ Photostream/UI/Photo Picker/PhotoPickerDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoPickerDelegate.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 11/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension PhotoPickerViewController: UICollectionViewDelegate {
+
+}
