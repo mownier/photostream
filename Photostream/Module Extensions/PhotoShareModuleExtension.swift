feed8553c47ef4680a5419a35ea1652edfd5ad93
Mounir Ybanez <rinuom91@gmail.com>
2016-11-18 15:49:12 +0800

    Added navigation from photo picker to photo share.

---
+++ Photostream/Module Extensions/PhotoShareModuleExtension.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoShareModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 18/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoShareModuleExtension: AnyObject {
+
+}
---
+++ Photostream/UI/Photo Share/PhotoShareViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoShareViewController.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 18/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class PhotoShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
