e5df8c668590bd5a2431801f4321936cad1c5483
Mounir Ybanez <rinuom91@gmail.com>
2016-11-22 16:06:54 +0800

    Added delegation of photo share and capture to post composer presenter.

--- Photostream/Module Extensions/PostComposerModuleExtension.swift
+++ Photostream/Module Extensions/PostComposerModuleExtension.swift
@@ -11,16 +11,42 @@ import UIKit
 extension PostComposerPresenter {
     
     func presentPhotoShare(with image: UIImage) {
-        wireframe.showPhotoShare(from: view.controller?.navigationController, with: image)
+        wireframe.showPhotoShare(from: view.controller?.navigationController, with: image, delegate: self)
+    }
+}
+
+extension PostComposerPresenter: PhotoCaptureModuleDelegate {
+    
+    func photoCaptureDidFinish(with image: UIImage?) {
+        guard image != nil else {
+            return
+        }
+        
+        presentPhotoShare(with: image!)
+    }
+    
+    func photoCaptureDidCanel() {
+        print("PostComposerPresenter: did cancel capture")
+    }
+}
+
+extension PostComposerPresenter: PhotoShareModuleDelegate {
+    
+    func photoShareDidFinish(with image: UIImage, content: String) {
+        print("PostComposerPresenter: did finish share")
+    }
+    
+    func photoShareDidCancel() {
+        print("PostComposerPresenter: did cancel share")
     }
 }
 
 extension PostComposerWireframeInterface {
     
-    func showPhotoShare(from navigationController: UINavigationController?, with image: UIImage) {
+    func showPhotoShare(from navigationController: UINavigationController?, with image: UIImage, delegate: PhotoShareModuleDelegate) {
         let vc = PhotoShareWireframe.createViewController()
         vc.image = image
-        let wireframe = PhotoShareWireframe(root: root, view: vc)
+        let wireframe = PhotoShareWireframe(root: root, delegate: delegate, view: vc)
         wireframe.push(with: vc, from: navigationController)
     }
 }
