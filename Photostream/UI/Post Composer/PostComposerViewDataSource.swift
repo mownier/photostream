2699536977aae61881545476c7a73ebd7b2318f6
Mounir Ybanez <rinuom91@gmail.com>
2016-11-22 14:06:25 +0800

    Separated files for the data source and delegate.

---
+++ Photostream/UI/Post Composer/PostComposerViewDataSource.swift
@@ -0,0 +1,30 @@
+//
+//  PostComposerViewDataSource.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 22/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension PostComposerViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.index(of: viewController), currentIndex < pages.count - 1 else {
+            return nil
+        }
+        
+        let nextIndex = currentIndex + 1
+        return pages[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let currentIndex = pages.index(of: viewController), currentIndex > 0 else {
+            return nil
+        }
+        
+        let previousIndex = currentIndex - 1
+        return pages[previousIndex]
+    }
+}
---
+++ Photostream/UI/Post Composer/PostComposerViewDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  PostComposerViewDelegate.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 22/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension PostComposerViewController: UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        guard previousViewControllers.count > 0, completed,
+            let previousIndex = pages.index(of: previousViewControllers[0]),
+            pages[previousIndex] != pageViewController.topViewController else {
+                return
+        }
+        
+        let currentIndex = previousIndex == 0 ? 1 : 0
+        switch currentIndex {
+        case 0:
+            presenter.willShowLibrary()
+        case 1:
+            presenter.willShowCamera()
+        default:
+            break
+        }
+    }
+}
