d5bef514cae012685a9a99f1048d9c165eb102e5
Mounir Ybanez <rinuom91@gmail.com>
2016-11-23 12:28:32 +0800

    Added computing of square crop region.

--- Photostream/Modules/Photo Capture/Presenter/PhotoCapturePresenter.swift
+++ Photostream/Modules/Photo Capture/Presenter/PhotoCapturePresenter.swift
@@ -46,9 +46,7 @@ extension PhotoCapturePresenter: PhotoCaptureModuleInterface {
         camera = GPUImageStillCamera(sessionPreset: preset, cameraPosition: cameraPosition)
         camera?.outputImageOrientation = outputOrientation
         
-        let cropFilter = GPUImageCropFilter()
-        cropFilter.cropRegion = CGRect(x: 0, y: 0.125, width: 1, height: 0.75)
-        filter = cropFilter
+        filter = GPUImageCropFilter(cropRegion: squareCropRegion(for: preset))
         filter?.addTarget(preview)
         camera?.addTarget(filter)
     }
@@ -67,5 +65,32 @@ extension PhotoCapturePresenter: PhotoCaptureModuleInterface {
         }
         
         camera?.stopCapture()
+    }
+}
+
+extension PhotoCapturePresenter {
+    
+    func squareCropRegion(for preset: String) -> CGRect {
+        switch preset {
+        case AVCaptureSessionPreset640x480:
+            return squareCropRegion(for: CGSize(width: 640, height: 480))
+        case AVCaptureSessionPreset352x288:
+            return squareCropRegion(for: CGSize(width: 352, height: 288))
+        case AVCaptureSessionPreset1280x720:
+            return squareCropRegion(for: CGSize(width: 1280, height: 720))
+        case AVCaptureSessionPreset1920x1080:
+            return squareCropRegion(for: CGSize(width: 1920, height: 1080))
+        case AVCaptureSessionPreset3840x2160:
+            return squareCropRegion(for: CGSize(width: 3480, height: 2160))
+        default:
+            return CGRect(x: 0, y: 0, width: 1, height: 1)
+        }
+    }
+    
+    func squareCropRegion(for size: CGSize) -> CGRect {
+        var region = CGRect(x: 0, y: 0, width: 1, height: 1)
+        region.origin.y = (size.width - size.height) / size.width / 2
+        region.size.height = size.height / size.width
+        return region
     }
 }
