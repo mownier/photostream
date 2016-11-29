97e7ae5631c78c27682ee1600c12ebad8a085249
Mounir Ybanez <rinuom91@gmail.com>
2016-11-29 14:01:11 +0800

    Added extension for comment feed module.

---
+++ Photostream/Module Extensions/CommentFeedExtension.swift
@@ -0,0 +1,14 @@
+//
+//  CommentFeedExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 29/11/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+extension CommentFeedModule {
+    
+    convenience init() {
+        self.init(view: CommentFeedViewController())
+    }
+}
