31d6be46fead1f6d2cd45dc1cff0aa942a920074
Mounir Ybanez <rinuom91@gmail.com>
2016-12-03 15:03:54 +0800

    Refactored comment and user snapshot parser.

--- Photostream/Services/ServiceProvider/CommentServiceProvider.swift
+++ Photostream/Services/ServiceProvider/CommentServiceProvider.swift
@@ -40,23 +40,18 @@ struct CommentServiceProvider: CommentService {
             var commentList = [Comment]()
             var commentUsers = [String: User]()
             for snap in data.children {
-                comments.child((snap as AnyObject).key).observeSingleEvent(of: .value, with: { (data2) in
-                    let userId = data2.childSnapshot(forPath: "uid").value as! String
-                    users.child(userId).observeSingleEvent(of: .value, with: { (data3) in
+                comments.child((snap as AnyObject).key).observeSingleEvent(of: .value, with: { (commentSnapshot) in
+                    guard let userId = commentSnapshot.childSnapshot(forPath: "uid").value as? String else {
+                        return
+                    }
+                    
+                    users.child(userId).observeSingleEvent(of: .value, with: { (userSnapshot) in
                         if commentUsers[userId] == nil {
-                            var user = User()
-                            user.id = userId
-                            user.firstName = data3.childSnapshot(forPath: "firstname").value as! String
-                            user.lastName = data3.childSnapshot(forPath: "lastname").value as! String
-                            commentUsers[userId] = user
+                            let user = User(with: userSnapshot, exception: "email")
+                            commentUsers[user.id] = user
                         }
                         
-                        var comment = Comment()
-                        comment.userId = userId
-                        comment.id = data2.childSnapshot(forPath: "id").value as! String
-                        comment.message = data2.childSnapshot(forPath: "message").value as! String
-                        comment.timestamp = (data2.childSnapshot(forPath: "timestamp").value as! Double) / 1000
-                        
+                        let comment = Comment(with: commentSnapshot)
                         commentList.append(comment)
                         
                         if UInt(commentList.count) == data.childrenCount {
@@ -96,18 +91,10 @@ struct CommentServiceProvider: CommentService {
         let updates: [String: AnyObject] = [path1: data as AnyObject, path2: true as AnyObject, path3: true as AnyObject]
         
         ref.updateChildValues(updates)
-        ref.child(path1).observeSingleEvent(of: .value, with: { (data) in
-            ref.child("users/\(userId)").observeSingleEvent(of: .value, with: { (data2) in
-                var user = User()
-                user.id = userId
-                user.firstName = data2.childSnapshot(forPath: "firstname").value as! String
-                user.lastName = data2.childSnapshot(forPath: "lastname").value as! String
-                
-                var comment = Comment()
-                comment.id = key
-                comment.message = message
-                comment.userId = userId
-                comment.timestamp = data.childSnapshot(forPath: "timestamp").value as! Double
+        ref.child(path1).observeSingleEvent(of: .value, with: { (commentSnapshot) in
+            ref.child("users/\(userId)").observeSingleEvent(of: .value, with: { (userSnapshot) in
+                let user = User(with: userSnapshot, exception: "email")
+                let comment = Comment(with: commentSnapshot)
                 
                 var comments = [Comment]()
                 comments.append(comment)
