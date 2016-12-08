a5cc8adf40e71acfeaaafc8ad74bb0d0fcf5d1d5
Mounir Ybanez <rinuom91@gmail.com>
2016-12-08 14:16:04 +0800

    Added PostListCollectionCellItem implementation.

--- Photostream/Module Extensions/UserPostModuleExtension.swift
+++ Photostream/Module Extensions/UserPostModuleExtension.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
 
+import DateTools
+
 enum UserPostSceneType {
     case grid
     case list
@@ -24,3 +26,44 @@ extension UserPostModule {
 }
 
 extension UserPostDataItem: PostGridCollectionCellItem { }
+
+extension UserPostDataItem: PostListCollectionCellItem {
+    
+    var timeAgo: String {
+        let date = NSDate(timeIntervalSince1970: timestamp)
+        return date.timeAgoSinceNow()
+    }
+    
+    var photoSize: CGSize {
+        var size = CGSize.zero
+        size.width = CGFloat(photoWidth)
+        size.height = CGFloat(photoHeight)
+        return size
+    }
+    
+    var likesText: String {
+        guard likes > 0 else {
+            return ""
+        }
+        
+        if likes > 1 {
+            return "\(likes) likes"
+            
+        } else {
+            return "1 like"
+        }
+    }
+    
+    var commentsText: String {
+        guard comments > 0 else {
+            return ""
+        }
+        
+        if comments > 1 {
+            return "View \(comments) comments"
+            
+        } else {
+            return "View 1 comment"
+        }
+    }
+}
