077edde243d3dfcf93b87d80e788dd3ccd9874d7
Mounir Ybanez <rinuom91@gmail.com>
2016-12-08 15:34:57 +0800

    Added post list collection header.

---
+++ Photostream/UI/Shared/PostListCollectionHeader/PostListCollectionHeader.swift
@@ -0,0 +1,98 @@
+//
+//  PostListCollectionHeader.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 08/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class PostListCollectionHeader: UICollectionReusableView {
+    
+    var avatarImageView: UIImageView!
+    var displayNameLabel: UILabel!
+    var stripView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSetup()
+    }
+    
+    func initSetup() {
+        backgroundColor = UIColor.white
+        
+        avatarImageView = UIImageView()
+        avatarImageView.contentMode = .scaleAspectFill
+        avatarImageView.backgroundColor = UIColor.lightGray
+        avatarImageView.cornerRadius = avatarDimension / 2
+        
+        displayNameLabel = UILabel()
+        
+        stripView = UIView()
+        stripView.backgroundColor = UIColor.lightGray
+        
+        addSubview(avatarImageView)
+        addSubview(displayNameLabel)
+        addSubview(stripView)
+    }
+    
+    override func layoutSubviews() {
+        var rect = CGRect.zero
+        
+        rect.origin.x = spacing * 2
+        rect.origin.y = (frame.size.height - avatarDimension) / 2
+        rect.size.width = avatarDimension
+        rect.size.height = avatarDimension
+        avatarImageView.frame = rect
+        
+        rect.origin.x += spacing
+        rect.origin.x += rect.size.width
+        rect.size.width = frame.size.width
+        rect.size.width -= rect.origin.x
+        rect.size.width -= (spacing * 2)
+        rect.size.height -= displayNameLabel.sizeThatFits(.zero).height
+        rect.origin.y = (frame.size.height - rect.size.height) / 2
+        displayNameLabel.frame = rect
+        
+        rect.origin.x = 0
+        rect.origin.y = frame.size.height - 1
+        rect.size.width = frame.size.width
+        rect.size.height = 1
+        stripView.frame = rect
+    }
+}
+
+extension PostListCollectionHeader {
+    
+    var avatarDimension: CGFloat {
+        return 32
+    }
+    
+    var spacing: CGFloat {
+        return 4
+    }
+}
+
+extension PostListCollectionHeader {
+    
+    static var reuseId: String {
+        return "PostListCollectionHeader"
+    }
+    
+    class func register(in view: UICollectionView) {
+        let kind = UICollectionElementKindSectionHeader
+        view.register(self, forSupplementaryViewOfKind: kind, withReuseIdentifier: self.reuseId)
+    }
+    
+    class func dequeue(from view: UICollectionView, for indexPath: IndexPath) -> PostListCollectionHeader?  {
+        let kind = UICollectionElementKindSectionHeader
+        let header = view.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: self.reuseId, for: indexPath)
+        return header as? PostListCollectionHeader
+    }
+}
---
+++ Photostream/UI/Shared/PostListCollectionHeader/PostListCollectionHeaderItem.swift
@@ -0,0 +1,57 @@
+//
+//  PostListCollectionHeaderItem.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 08/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import Kingfisher
+
+protocol PostListCollectionHeaderItem {
+
+    var avatarUrl: String { get }
+    var displayName: String { get }
+}
+
+protocol PostListCollectionHeaderConfig {
+    
+    func configure(with item: PostListCollectionHeaderItem?, isPrototype: Bool)
+    func set(avatar url: String, placeholder image: UIImage?)
+}
+
+extension PostListCollectionHeader: PostListCollectionHeaderConfig {
+    
+    func configure(with item: PostListCollectionHeaderItem?, isPrototype: Bool = false) {
+        guard item != nil else {
+            return
+        }
+        
+        if !isPrototype {
+            let image = createAvatarPlaceholderImage(with: item!.displayName[0])
+            set(avatar: item!.avatarUrl, placeholder: image)
+        }
+        
+        displayNameLabel.text = item!.displayName
+    }
+    
+    func set(avatar url: String, placeholder image: UIImage? = nil) {
+        guard let downloadUrl = URL(string: url) else {
+            avatarImageView.image = image
+            return
+        }
+        
+        let resource = ImageResource(downloadURL: downloadUrl)
+        avatarImageView.kf.setImage(with: resource, placeholder: image, options: nil, progressBlock: nil, completionHandler: nil)
+    }
+    
+    private func createAvatarPlaceholderImage(with initial: String) -> UIImage {
+        let width: CGFloat = avatarDimension
+        let height: CGFloat = avatarDimension
+        let frame = CGRect(x: 0, y: 0, width: width, height: height)
+        let font = UIFont.systemFont(ofSize: 15)
+        let text: String = initial
+        let placeholderImage = UILabel.createPlaceholderImageWithFrame(frame, text: text, font: font)
+        return placeholderImage
+    }
+}
