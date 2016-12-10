e5e308c4b6ca015766991439e84a90dc6ef92245
Mounir Ybanez <rinuom91@gmail.com>
2016-12-10 13:27:40 +0800

    Added user profile scene.

---
+++ Photostream/Modules/User Profile/View/UserProfileScene.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfileScene.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 10/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+protocol UserProfileScene: BaseModuleView {
+
+    var presenter: UserProfileModuleInterface! { set get }
+    
+    func didFetchUserProfile(with error: String?)
+    func didFetchUserProfile(with data: UserProfileData)
+}
