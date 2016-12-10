f3216ff4a48ec4e19cf25261badcadb562f328b2
Mounir Ybanez <rinuom91@gmail.com>
2016-12-10 13:28:19 +0800

    Added user profile module interface.

---
+++ Photostream/Modules/User Profile/Module/UserProfileModule.swift
@@ -0,0 +1,12 @@
+//
+//  UserProfileModule.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 10/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+protocol UserProfileModuleInterface: BaseModuleInterface {
+    
+    func fetchUserProfile()
+}
