34c189be52e1e8d8bdaef4865bba82411e26c485
Mounir Ybanez <rinuom91@gmail.com>
2016-12-10 13:29:52 +0800

    Added user profile wireframe.

--- Photostream/Modules/User Profile/Wireframe/UserProfileWireframe.swift
+++ Photostream/Modules/User Profile/Wireframe/UserProfileWireframe.swift
@@ -2,37 +2,18 @@
 //  UserProfileWireframe.swift
 //  Photostream
 //
-//  Created by Mounir Ybanez on 26/08/2016.
+//  Created by Mounir Ybanez on 10/12/2016.
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
 
-import UIKit
+protocol UserProfileWireframeInterface: BaseModuleWireframe { }
 
-class UserProfileWireframe: AnyObject {
+class UserProfileWireframe: UserProfileWireframeInterface {
 
-    weak var userProfileViewController: UserProfileViewController!
-    var userProfilePresenter: UserProfilePresenter!
-    var rootWireframe: RootWireframeInterface!
-
-    init(userId: String?) {
-//        let session = AuthSession()
-//        let user = UserServiceProvider(session: session)
-//        let post = PostServiceProvider(session: session)
-//        let service = UserProfileService(user: user, post: post)
-//        let interactor = UserProfileInteractor(service: service, userId: userId)
-//        let presenter = UserProfilePresenter()
-//        interactor.output = presenter
-//        presenter.interactor = interactor
-//        presenter.wireframe = self
-//
-//        self.userProfilePresenter = presenter
-    }
-
-    func navigateCommentsInterface(_ postId: String, shouldComment: Bool) {
-
-    }
-
-    func navigateLikesInterface(_ postId: String) {
-
+    var style: WireframeStyle!
+    var root: RootWireframe?
+    
+    required init(root: RootWireframe?) {
+        self.root = root
     }
 }
