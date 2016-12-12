43301e3355c3879d9134d4a26532b4f603f49c10
Mounir Ybanez <rinuom91@gmail.com>
2016-12-12 15:48:46 +0800

    Added snapshot parser for profile.

--- Photostream/Services/Models/Profile.swift
+++ Photostream/Services/Models/Profile.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
 
-import Foundation
+import Firebase
 
 struct Profile {
 
@@ -22,5 +22,28 @@ struct Profile {
         postsCount = 0
         followersCount = 0
         followingCount = 0
+    }
+}
+
+extension Profile: SnapshotParser {
+    
+    init(with snapshot: FIRDataSnapshot, exception: String...) {
+        self.init()
+        
+        if snapshot.hasChild("uid") && !exception.contains("uid") {
+            userId = snapshot.childSnapshot(forPath: "uid").value as! String
+        }
+    
+        if snapshot.hasChild("posts_count") && !exception.contains("posts_count") {
+            postsCount = snapshot.childSnapshot(forPath: "posts_count").value as! Int
+        }
+        
+        if snapshot.hasChild("followers_count") && !exception.contains("followers_count") {
+            followersCount = snapshot.childSnapshot(forPath: "followers_count").value as! Int
+        }
+        
+        if snapshot.hasChild("following_count") && !exception.contains("following_count") {
+            followingCount = snapshot.childSnapshot(forPath: "following_count").value as! Int
+        }
     }
 }
