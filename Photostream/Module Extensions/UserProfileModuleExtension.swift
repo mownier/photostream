87a423df0a1506a4ed2ac64c9d04141db2b00c3c
Mounir Ybanez <rinuom91@gmail.com>
2016-12-12 15:51:02 +0800

    Added user profile module extension.

--- Photostream/Module Extensions/UserProfileModuleExtension.swift
+++ Photostream/Module Extensions/UserProfileModuleExtension.swift
@@ -5,3 +5,30 @@
 //  Created by Mounir Ybanez on 10/12/2016.
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
+
+extension UserProfileModule {
+    
+    convenience init() {
+        self.init(view: UserProfileViewController())
+    }
+}
+
+extension UserProfileDataItem: UserProfileViewItem {
+    
+    var postCountText: String {
+        return "\(postCount)"
+    }
+    
+    var followerCountText: String {
+        return "\(followerCount)"
+    }
+    
+    var followingCountText: String {
+        return "\(followingCount)"
+    }
+    
+    var isMe: Bool {
+        let session = AuthSession()
+        return session.user.id == id
+    }
+}
