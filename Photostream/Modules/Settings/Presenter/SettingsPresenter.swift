c5a7232deb91517f8458b644cb1c564198e594fc
Mounir Ybanez <rinuom91@gmail.com>
2016-12-14 14:41:59 +0800

    Implemented settings section.

--- Photostream/Modules/Settings/Presenter/SettingsPresenter.swift
+++ Photostream/Modules/Settings/Presenter/SettingsPresenter.swift
@@ -8,6 +8,7 @@
 
 protocol SettingsPresenterInterface: BaseModulePresenter {
     
+    var sections: [SettingSection] { set get }
 }
 
 class SettingsPresenter: SettingsPresenterInterface {
@@ -15,10 +16,46 @@ class SettingsPresenter: SettingsPresenterInterface {
     typealias ModuleView = SettingsScene
     typealias ModuleWireframe = SettingsWireframeInterface
     
+    lazy var sections = [SettingSection]()
+    
     var view: ModuleView!
     var wireframe: ModuleWireframe!
 }
 
-extension SettingsPresenter {
+extension SettingsPresenter: SettingsModuleInterface {
     
+    var sectionCount: Int {
+        return sections.count
+    }
+    
+    func itemCount(for section: Int) -> Int {
+        guard sections.isValid(section) else {
+            return 0
+        }
+        
+        return sections[section].items.count
+    }
+    
+    func itemName(at index: Int, for section: Int) -> String {
+        guard sections.isValid(section),
+            sections[section].items.isValid(index) else {
+            return ""
+        }
+        
+        return sections[section].items[index]
+    }
+    
+    func sectionName(at index: Int) -> String {
+        guard sections.isValid(index) else {
+            return ""
+        }
+        
+        return sections[index].name
+    }
+    
+    func exit() {
+        var property = WireframeExitProperty()
+        property.controller = view.controller
+        wireframe.exit(with: property)
+    }
 }
