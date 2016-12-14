888d5922eef2ccb8a92cc6e54a488707ee090222
Mounir Ybanez <rinuom91@gmail.com>
2016-12-14 15:35:27 +0800

    Added showing of login.

--- Photostream/Module Extensions/SettingsModuleExtension.swift
+++ Photostream/Module Extensions/SettingsModuleExtension.swift
@@ -6,9 +6,38 @@
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
 
+import UIKit
+import FirebaseAuth
+
 extension SettingsModule {
     
     convenience init() {
         self.init(view: SettingsViewController())
+    }
+}
+
+extension SettingsModuleInterface {
+    
+    func presentLogin() {
+        guard let presenter = self as? SettingsPresenter else {
+            return
+        }
+        
+        try? FIRAuth.auth()?.signOut()
+        
+        presenter.wireframe.showLogin()
+    }
+}
+
+extension SettingsWireframeInterface {
+    
+    func showLogin() {
+        guard root != nil else {
+            return
+        }
+        
+        let vc = LoginWireframe.createViewController()
+        let loginWireframe = LoginWireframe(root: root as? RootWireframeInterface, view: vc)
+        loginWireframe.attachRoot(with: vc, in: root!.window)
     }
 }
