2efe14a9b169d0d337f38e501935bd5d00482148
Mounir Ybanez <rinuom91@gmail.com>
2016-12-14 14:41:09 +0800

    Added settings section struct.

---
+++ Photostream/Modules/Settings/Presenter/SettingSection.swift
@@ -0,0 +1,13 @@
+//
+//  SettingSection.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 14/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+struct SettingSection {
+    
+    var name: String = ""
+    var items = [String]()
+}
