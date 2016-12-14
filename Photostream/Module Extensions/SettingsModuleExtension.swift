75cc20b4852c52c4ca498a80a8e008e67f524997
Mounir Ybanez <rinuom91@gmail.com>
2016-12-14 15:03:09 +0800

    Added settings module extension.

---
+++ Photostream/Module Extensions/SettingsModuleExtension.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 14/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+extension SettingsModule {
+    
+    convenience init() {
+        self.init(view: SettingsViewController())
+    }
+}
