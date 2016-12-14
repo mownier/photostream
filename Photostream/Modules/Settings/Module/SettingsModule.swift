f07977caddb9632b9a744b141335eea641dfe69e
Mounir Ybanez <rinuom91@gmail.com>
2016-12-14 14:57:25 +0800

    Added building of settings module.

--- Photostream/Modules/Settings/Module/SettingsModule.swift
+++ Photostream/Modules/Settings/Module/SettingsModule.swift
@@ -14,3 +14,41 @@ protocol SettingsModuleInterface: BaseModuleInterface {
     func itemCount(for section: Int) -> Int
     func itemName(at index: Int, for section: Int) -> String
 }
+
+protocol SettingsModuleBuilder: BaseModuleBuilder {
+    
+    func build(root: RootWireframe?, sections: [SettingSection])
+}
+
+class SettingsModule: BaseModule {
+    
+    typealias ModuleView = SettingsScene
+    typealias ModulePresenter = SettingsPresenter
+    typealias ModuleWireframe = SettingsWireframe
+    
+    var view: ModuleView!
+    var presenter: ModulePresenter!
+    var wireframe: ModuleWireframe!
+    
+    required init(view: ModuleView) {
+        self.view = view
+    }
+}
+
+extension SettingsModule: SettingsModuleBuilder {
+    
+    func build(root: RootWireframe?) {
+        presenter = SettingsPresenter()
+        wireframe = SettingsWireframe(root: root)
+        
+        view.presenter = presenter
+        
+        presenter.view = view
+        presenter.wireframe = wireframe
+    }
+    
+    func build(root: RootWireframe?, sections: [SettingSection]) {
+        build(root: root)
+        presenter.sections.append(contentsOf: sections)
+    }
+}
