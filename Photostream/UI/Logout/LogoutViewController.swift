b646d4f792e8673c5104c9c3163c5ba34ca4e606
Mounir Ybanez <rinuom91@gmail.com>
2016-12-15 11:29:56 +0800

    Added UI for logout module.

---
+++ Photostream/UI/Logout/LogoutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LogoutViewController.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 15/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class LogoutViewController: UIAlertController {
+    
+    var presenter: LogoutModuleInterface!
+    
+    override func loadView() {
+        super.loadView()
+        
+        var action = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        addAction(action)
+        
+        action = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.presenter.logout()
+        }
+        
+        addAction(action)
+    }
+}
+
+extension LogoutViewController: LogoutScene {
+    
+    var controller: UIViewController? {
+        return self
+    }
+    
+    func didLogout(with error: String?) {
+        
+    }
+}
