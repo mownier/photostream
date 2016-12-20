afe728201fd497565aa9a61b7d09358e5c667594
Mounir Ybanez <rinuom91@gmail.com>
2016-12-20 14:28:55 +0800

    Added UI for post discovery.

---
+++ Photostream/UI/Post Discovery/PostDiscoveryViewController.swift
@@ -0,0 +1,218 @@
+//
+//  PostDiscoveryViewController.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 20/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+enum PostDiscoverySceneType {
+    case grid
+    case list
+}
+
+class PostDiscoveryViewController: UICollectionViewController {
+
+    weak var scrollEventListener: ScrollEventListener?
+    
+    var presenter: PostDiscoveryModuleInterface!
+    
+    lazy var gridLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    lazy var listLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    lazy var prototype: PostListCollectionCell! = PostListCollectionCell()
+    
+    lazy var emptyView: GhostView! = {
+        let view = GhostView()
+        view.titleLabel.text = "No posts"
+        return view
+    }()
+    
+    lazy var loadingView: UIActivityIndicatorView! = {
+        let view = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        view.hidesWhenStopped = true
+        return view
+    }()
+    
+    lazy var refreshView: UIRefreshControl! = {
+        let view = UIRefreshControl()
+        view.addTarget(
+            self,
+            action: #selector(self.triggerRefresh),
+            for: .valueChanged)
+        return view
+    }()
+    
+    lazy var scrollHandler: ScrollHandler = {
+        var handler = ScrollHandler()
+        handler.scrollView = self.collectionView
+        return handler
+    }()
+    
+    var sceneType: PostDiscoverySceneType = .list {
+        didSet {
+            guard sceneType != oldValue else {
+                return
+            }
+            
+            reloadView()
+            collectionView!.collectionViewLayout.invalidateLayout()
+            
+            switch sceneType {
+            case .grid:
+                collectionView!.setCollectionViewLayout(gridLayout, animated: false)
+                
+            case .list:
+                collectionView!.setCollectionViewLayout(listLayout, animated: false)
+            }
+        }
+    }
+    
+    var shouldShowLoadingView: Bool = false {
+        didSet {
+            guard shouldShowLoadingView != oldValue else {
+                return
+            }
+            
+            if shouldShowLoadingView {
+                loadingView.startAnimating()
+                collectionView?.backgroundView = loadingView
+            } else {
+                loadingView.stopAnimating()
+                loadingView.removeFromSuperview()
+                collectionView?.backgroundView = nil
+            }
+        }
+    }
+    
+    var shouldShowRefreshView: Bool = false {
+        didSet {
+            if refreshView.superview == nil {
+                collectionView?.addSubview(refreshView)
+            }
+            
+            if shouldShowLoadingView {
+                refreshView.beginRefreshing()
+            } else {
+                refreshView.endRefreshing()
+            }
+        }
+    }
+    
+    var shouldShowEmptyView: Bool = false {
+        didSet {
+            guard shouldShowEmptyView != oldValue, collectionView != nil else {
+                return
+            }
+            
+            if shouldShowEmptyView {
+                emptyView.frame.size = collectionView!.frame.size
+                collectionView!.backgroundView = emptyView
+            } else {
+                collectionView!.backgroundView = nil
+                emptyView.removeFromSuperview()
+            }
+        }
+    }
+    
+    convenience init(type: PostDiscoverySceneType) {
+        let layout = UICollectionViewFlowLayout()
+        self.init(collectionViewLayout: layout)
+        switch type {
+        case .grid:
+            self.gridLayout = layout
+        case .list:
+            self.listLayout = layout
+        }
+        self.sceneType = type
+    }
+    
+    override func loadView() {
+        super.loadView()
+        
+        collectionView!.alwaysBounceVertical = true
+        collectionView!.backgroundColor = UIColor.white
+        
+        let size = collectionView!.frame.size
+        
+        gridLayout.configure(with: size.width, columnCount: 3)
+        
+        listLayout.configure(with: size.width, columnCount: 1)
+        listLayout.headerReferenceSize = CGSize(width: size.width, height: 48)
+        listLayout.sectionHeadersPinToVisibleBounds = false
+        
+        PostGridCollectionCell.register(in: collectionView!)
+        PostListCollectionCell.register(in: collectionView!)
+        PostListCollectionHeader.register(in: collectionView!)
+        
+        prototype.bounds.size.width = size.width
+        
+        setupNavigationItem()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presenter.initialLoad()
+    }
+    
+    func triggerRefresh() {
+        presenter.refreshPosts()
+    }
+    
+    func setupNavigationItem() {
+        navigationItem.title = "Discovery"
+    }
+}
+
+extension PostDiscoveryViewController: PostDiscoveryScene {
+    
+    var controller: UIViewController? {
+        return self
+    }
+    
+    func reloadView() {
+        collectionView?.reloadData()
+    }
+    
+    func showEmptyView() {
+        shouldShowEmptyView = true
+    }
+    
+    func showRefreshView() {
+        shouldShowRefreshView = true
+    }
+    
+    func showInitialLoadView() {
+        shouldShowLoadingView = true
+    }
+    
+    func hideEmptyView() {
+        shouldShowEmptyView = false
+    }
+    
+    func hideRefreshView() {
+        shouldShowRefreshView = false
+    }
+    
+    func hideInitialLoadView() {
+        shouldShowLoadingView = false
+    }
+    
+    func didRefresh(with error: String?) {
+        
+    }
+    
+    func didLoadMore(with error: String?) {
+        
+    }
+    
+    func didLike(with error: String?) {
+        
+    }
+    
+    func didUnlike(with error: String?) {
+        
+    }
+}
---
+++ Photostream/UI/Post Discovery/PostDiscoveryViewDelegate.swift
@@ -0,0 +1,103 @@
+//
+//  PostDiscoveryViewDelegate.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 20/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension PostDiscoveryViewController: UICollectionViewDelegateFlowLayout {
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        var condition: Bool = false
+        
+        switch sceneType {
+        case .grid:
+            condition = indexPath.row == presenter.postCount - 10
+            
+        case .list:
+            condition = indexPath.section == presenter.postCount - 10
+        }
+        
+        guard condition else {
+            return
+        }
+        
+        presenter.loadMorePosts()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch sceneType {
+        case .grid:
+            return gridLayout.itemSize
+            
+        case .list:
+            let item = presenter.post(at: indexPath.section) as? PostListCollectionCellItem
+            prototype.configure(with: item, isPrototype: true)
+            let size = CGSize(width: listLayout.itemSize.width, height: prototype.dynamicHeight)
+            return size
+        }
+    }
+}
+
+extension PostDiscoveryViewController: PostListCollectionCellDelegate {
+    
+    func didTapPhoto(cell: PostListCollectionCell) {
+        guard let index = collectionView![cell]?.section else {
+            return
+        }
+        
+        presenter.likePost(at: index)
+    }
+    
+    func didTapHeart(cell: PostListCollectionCell) {
+        guard let index = collectionView![cell]?.section,
+            let post = presenter.post(at: index) else {
+                return
+        }
+        
+        cell.toggleHeart(liked: post.isLiked) { [unowned self] in
+            self.presenter.toggleLike(at: index)
+        }
+    }
+    
+    func didTapComment(cell: PostListCollectionCell) {
+        guard let index = collectionView![cell]?.section else {
+            return
+        }
+        
+        presenter.presentCommentController(at: index, shouldComment: true)
+    }
+    
+    func didTapCommentCount(cell: PostListCollectionCell) {
+        guard let index = collectionView![cell]?.section else {
+            return
+        }
+        
+        presenter.presentCommentController(at: index)
+    }
+    
+    func didTapLikeCount(cell: PostListCollectionCell) {
+        
+    }
+}
+
+extension PostDiscoveryViewController {
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollHandler.isScrollable {
+            if scrollHandler.isScrollingUp {
+                scrollEventListener?.didScrollUp(
+                    with: scrollHandler.offsetDelta,
+                    offsetY: scrollHandler.currentOffsetY )
+            } else if scrollHandler.isScrollingDown {
+                scrollEventListener?.didScrollDown(
+                    with: scrollHandler.offsetDelta,
+                    offsetY: scrollHandler.currentOffsetY)
+            }
+            scrollHandler.update()
+        }
+    }
+}
