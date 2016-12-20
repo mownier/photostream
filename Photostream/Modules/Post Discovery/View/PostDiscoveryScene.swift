685cd620d7beccb41780a1a1f6e6387061a9e3af
Mounir Ybanez <rinuom91@gmail.com>
2016-12-20 13:47:06 +0800

    Added post module scene.

---
+++ Photostream/Modules/Post Discovery/View/PostDiscoveryScene.swift
@@ -0,0 +1,12 @@
+//
+//  PostDiscoveryScene.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 20/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+protocol PostDiscoveryScene: BaseModuleView {
+
+    var presenter: PostDiscoveryModuleInterface! { set get }
+}
