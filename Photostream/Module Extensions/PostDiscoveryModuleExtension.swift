922d0e9e18875264e7eb295163ebe4aa0127b65c
Mounir Ybanez <rinuom91@gmail.com>
2016-12-20 14:28:38 +0800

    Added post discovery module extension.

---
+++ Photostream/Module Extensions/PostDiscoveryModuleExtension.swift
@@ -0,0 +1,53 @@
+//
+//  PostDiscoveryModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 20/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+extension PostDiscoveryWireframeInterface {
+    
+    func presentCommentController(from parent: UIViewController, delegate: CommentControllerDelegate?, postId: String, shouldComment: Bool = false) {
+        let controller = CommentController(root: nil)
+        controller.postId = postId
+        controller.style = .push
+        controller.shouldComment = shouldComment
+        controller.delegate = delegate
+        
+        var property = WireframeEntryProperty()
+        property.controller = controller
+        property.parent = parent
+        
+        controller.enter(with: property)
+    }
+}
+
+extension PostDiscoveryModuleInterface {
+    
+    func presentCommentController(at index: Int, shouldComment: Bool = false) {
+        guard let presenter = self as? PostDiscoveryPresenter,
+            let parent = presenter.view.controller,
+            let post = post(at: index) else {
+                return
+        }
+        
+        presenter.wireframe.presentCommentController(from: parent, delegate: presenter, postId: post.id, shouldComment: shouldComment)
+    }
+}
+
+extension PostDiscoveryPresenter: CommentControllerDelegate {
+    
+    func commentControllerDidWrite(with postId: String) {
+        guard let index = indexOf(post: postId) else {
+            return
+        }
+        
+        var post = posts[index]
+        post.comments += 1
+        posts[index] = post
+        view.reloadView()
+    }
+}
