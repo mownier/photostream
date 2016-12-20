6653e1cc71ef6951fe22f2ffd4d84e07282bcdcb
Mounir Ybanez <rinuom91@gmail.com>
2016-12-20 13:06:06 +0800

    Added dictionary extension for converting from array to dictionary.

---
+++ Photostream/Utils/Extensions/DictionaryExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryExtensions.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 20/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+extension Dictionary {
+    
+    public init(keyValuePairs: [(Key, Value)]) {
+        self.init()
+        for pair in keyValuePairs {
+            self[pair.0] = pair.1
+        }
+    }
+}
