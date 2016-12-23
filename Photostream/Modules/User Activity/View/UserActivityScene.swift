02a43dce7e40cba89fe0fa4f595bf6dbea283f9f
Mounir Ybanez <rinuom91@gmail.com>
2016-12-23 14:42:04 +0800

    Added scene for user activity module.

---
+++ Photostream/Modules/User Activity/View/UserActivityScene.swift
@@ -0,0 +1,25 @@
+//
+//  UserActivityScene.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 23/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+protocol UserActivityScene: BaseModuleView {
+
+    var presnter: UserActivityModuleInterface! { set get }
+    
+    func reloadView()
+    
+    func showInitialLoadView()
+    func showRefreshView()
+    func showEmptyView()
+    
+    func hideInitialLoadView()
+    func hideRefreshView()
+    func hideEmptyView()
+    
+    func didRefresh(with error: String?)
+    func didLoadMore(with error: String?)
+}
