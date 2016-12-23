aa90225f016f6cf163fc05ca91d355ea7fab1670
Mounir Ybanez <rinuom91@gmail.com>
2016-12-23 15:03:56 +0800

    Added builder for user activity module.

--- Photostream/Modules/User Activity/Module/UserActivityModule.swift
+++ Photostream/Modules/User Activity/Module/UserActivityModule.swift
@@ -16,3 +16,47 @@ protocol UserActivityModuleInterface: BaseModuleInterface {
     
     func activity(at index: Int) -> UserActivityData?
 }
+
+protocol UserActivityBuilder: BaseModuleBuilder {
+    
+    func build(root: RootWireframe?, userId: String)
+}
+
+class UserActivityModule: BaseModule, BaseModuleInteractable {
+    
+    typealias ModuleView = UserActivityScene
+    typealias ModuleInteractor = UserActivityInteractor
+    typealias ModulePresenter = UserActivityPresenter
+    typealias ModuleWireframe = UserActivityWireframe
+    
+    var view: ModuleView!
+    var interactor: ModuleInteractor!
+    var presenter: ModulePresenter!
+    var wireframe: ModuleWireframe!
+    
+    required init(view: ModuleView) {
+        self.view = view
+    }
+}
+
+extension UserActivityModule: UserActivityBuilder {
+    
+    func build(root: RootWireframe?) {
+        let service = UserServiceProvider(session: AuthSession())
+        interactor = UserActivityInteractor(service: service)
+        presenter = UserActivityPresenter()
+        wireframe = UserActivityWireframe(root: root)
+        
+        view.presenter = presenter
+        interactor.output = presenter
+        
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.wireframe = wireframe
+    }
+    
+    func build(root: RootWireframe?, userId: String) {
+        build(root: root)
+        presenter.userId = userId
+    }
+}
