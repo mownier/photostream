bd2bd9b895f78910b0078f55df2613b8ccd65f8c
Mounir Ybanez <rinuom91@gmail.com>
2016-12-23 14:40:15 +0800

    Added module interface for user activity.

---
+++ Photostream/Modules/User Activity/Module/UserActivityModule.swift
@@ -0,0 +1,17 @@
+//
+//  UserActivityModule.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 23/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+protocol UserActivityModuleInterface: BaseModuleInterface {
+    
+    var activityCount: Int { get }
+    
+    func refreshActivities()
+    func loadMoreActivities()
+    
+    func activity(at index: Int) -> UserActivityData?
+}
