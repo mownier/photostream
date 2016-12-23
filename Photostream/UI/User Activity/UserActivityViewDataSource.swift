6ab44a70939e20aee02b191e80c944c747fa4337
Mounir Ybanez <rinuom91@gmail.com>
2016-12-23 15:52:28 +0800

    Added table cell configuration.

--- Photostream/UI/User Activity/UserActivityViewDataSource.swift
+++ Photostream/UI/User Activity/UserActivityViewDataSource.swift
@@ -13,4 +13,29 @@ extension UserActivityViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return presenter.activityCount
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ActivityTableCell.dequeue(from: tableView)!
+        let item = presenter.activity(at: indexPath.row)!
+        
+        switch item {
+            
+        case let likeItem as ActivityTableCellLikeItem:
+            cell.configure(with: likeItem)
+        
+        case let postItem as ActivityTableCellPostItem:
+            cell.configure(with: postItem)
+        
+        case let commentItem as ActivityTableCellCommentItem:
+            cell.configure(with: commentItem)
+        
+        case let followItem as ActivityTableCellFollowItem:
+            cell.configure(with: followItem)
+        
+        default:
+            break
+        }
+        
+        return cell
+    }
 }
