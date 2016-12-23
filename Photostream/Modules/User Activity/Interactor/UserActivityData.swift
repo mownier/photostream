0f1ae08f99e793919fbe80bbcc0e6fdf90766ff4
Mounir Ybanez <rinuom91@gmail.com>
2016-12-23 14:37:18 +0800

    Added user activity data.

--- Photostream/Modules/User Activity/Interactor/UserActivityData.swift
+++ Photostream/Modules/User Activity/Interactor/UserActivityData.swift
@@ -7,9 +7,118 @@
 //
 
 protocol UserActivityData {
-
+    
+    var timestamp: Double { set get }
+    
+    var userId: String { set get }
+    var avatarUrl: String { set get }
+    var displayName: String { set get }
 }
 
-struct UserActivityDataItem: UserActivityData {
+
+protocol UserActivityLikeData: UserActivityData {
     
+    var photoUrl: String { set get }
+    var postId: String { set get }
+}
+
+protocol UserActivityPostData: UserActivityData {
+    
+    var photoUrl: String { set get }
+    var postId: String { set get }
+}
+
+protocol UserActivityCommentData: UserActivityData {
+    
+    var commentId: String { set get }
+    var message: String { set get }
+    
+    var photoUrl: String { set get }
+    var postId: String { set get }
+}
+
+protocol UserActivityFollowData: UserActivityData { }
+
+struct UserActivityLikeDataItem: UserActivityLikeData {
+    
+    var timestamp: Double = 0
+    
+    var userId: String = ""
+    var avatarUrl: String = ""
+    var displayName: String = ""
+    
+    var photoUrl: String = ""
+    var postId: String = ""
+    
+    init(user: User, post: Post) {
+        userId = user.id
+        avatarUrl = user.avatarUrl
+        displayName = user.displayName
+        
+        photoUrl = post.photo.url
+        postId = post.id
+    }
+}
+
+struct UserActivityPostDataItem: UserActivityLikeData {
+    
+    var timestamp: Double = 0
+    
+    var userId: String = ""
+    var avatarUrl: String = ""
+    var displayName: String = ""
+    
+    var photoUrl: String = ""
+    var postId: String = ""
+    
+    init(user: User, post: Post) {
+        userId = user.id
+        avatarUrl = user.avatarUrl
+        displayName = user.displayName
+        
+        photoUrl = post.photo.url
+        postId = post.id
+    }
+}
+
+struct UserActivityCommentDataItem: UserActivityCommentData {
+    
+    var timestamp: Double = 0
+    
+    var userId: String = ""
+    var avatarUrl: String = ""
+    var displayName: String = ""
+    
+    var commentId: String = ""
+    var message: String = ""
+    
+    var photoUrl: String = ""
+    var postId: String = ""
+    
+    init(user: User, comment: Comment, post: Post) {
+        userId = user.id
+        avatarUrl = user.avatarUrl
+        displayName = user.displayName
+        
+        commentId = comment.id
+        message = comment.message
+        
+        photoUrl = post.photo.url
+        postId = post.id
+    }
+}
+
+struct UserActivityFollowDataItem: UserActivityFollowData {
+    
+    var timestamp: Double = 0
+    
+    var userId: String = ""
+    var avatarUrl: String = ""
+    var displayName: String = ""
+    
+    init(user: User) {
+        userId = user.id
+        avatarUrl = user.avatarUrl
+        displayName = user.displayName
+    }
 }
