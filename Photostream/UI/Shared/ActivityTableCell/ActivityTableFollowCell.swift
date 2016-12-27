20626c148fcba8cae5dff2ec9decef25bb98aa2e
Mounir Ybanez <rinuom91@gmail.com>
2016-12-27 13:34:18 +0800

    Added activity follow cell.

---
+++ Photostream/UI/Shared/ActivityTableCell/ActivityTableFollowCell.swift
@@ -0,0 +1,118 @@
+//
+//  ActivityTableFollowCell.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 27/12/2016.
+//  Copyright © 2016 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTableFollowCell: UITableViewCell {
+
+    var avatarImageView: UIImageView!
+    var contentLabel: UILabel!
+    var actionButton: UIButton!
+    
+    convenience init() {
+        self.init(style: .default, reuseIdentifier: ActivityTableCommentCell.reuseId)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSetup()
+    }
+    
+    fileprivate func initSetup() {
+        avatarImageView = UIImageView()
+        avatarImageView.cornerRadius = avatarDimension / 2
+        avatarImageView.contentMode = .scaleAspectFill
+        avatarImageView.backgroundColor = .lightGray
+        
+        contentLabel = UILabel()
+        contentLabel.numberOfLines = 0
+        contentLabel.font = UIFont.systemFont(ofSize: 12)
+        
+        actionButton = UIButton()
+        actionButton.addTarget(self, action: #selector(self.didTapAction), for: .touchUpInside)
+        actionButton.setTitle("Action", for: .normal)
+        
+        addSubview(avatarImageView)
+        addSubview(contentLabel)
+        addSubview(actionButton)
+    }
+    
+    override func layoutSubviews() {
+        var rect = CGRect.zero
+        
+        rect.origin.x = spacing
+        rect.origin.y = (frame.height - avatarDimension) / 2
+        rect.size.width = avatarDimension
+        rect.size.height = avatarDimension
+        avatarImageView.frame = rect
+        
+        rect.origin.x = frame.width - actionButtonSize.width - spacing
+        rect.origin.y = (frame.height - actionButtonSize.height) / 2
+        rect.size = actionButtonSize
+        actionButton.frame = rect
+        
+        rect.origin.x = avatarImageView.frame.maxX
+        rect.origin.x += spacing
+        rect.size.width = frame.width - rect.origin.x
+        rect.size.width -= actionButton.frame.width
+        rect.size.width -= (spacing * 2)
+        rect.size.height = contentLabel.sizeThatFits(rect.size).height
+        rect.origin.y = (frame.height - rect.size.height) / 2
+        contentLabel.frame = rect
+    }
+}
+
+extension ActivityTableFollowCell {
+    
+    func didTapAction() {
+        
+    }
+}
+
+extension ActivityTableFollowCell {
+    
+    var spacing: CGFloat {
+        return 4
+    }
+    
+    var avatarDimension: CGFloat {
+        return 32
+    }
+    
+    var actionButtonSize: CGSize {
+        return CGSize(width: 32, height: 16)
+    }
+}
+
+extension ActivityTableFollowCell {
+    
+    static var reuseId: String {
+        return "ActivityTableFollowCell"
+    }
+}
+
+extension ActivityTableFollowCell {
+    
+    class func dequeue(from view: UITableView) -> ActivityTableFollowCell? {
+        return view.dequeueReusableCell(withIdentifier: self.reuseId) as? ActivityTableFollowCell
+    }
+    
+    class func register(in view: UITableView) {
+        view.register(self, forCellReuseIdentifier: self.reuseId)
+    }
+}
+
+
+
+
+
