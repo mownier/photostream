b3436f8eac4b3f31afae60bce0ad28b486da2b40
Mounir Ybanez <rinuom91@gmail.com>
2016-12-29 15:56:08 +0800

    Implemented photo library change observer.

--- Photostream/Local Services/Service Provider/AssetServiceProvider.swift
+++ Photostream/Local Services/Service Provider/AssetServiceProvider.swift
@@ -9,8 +9,22 @@
 import UIKit
 import Photos
 
-class AssetServiceProvider: AssetService {
+class AssetServiceProvider: NSObject, AssetService {
 
+    fileprivate var fetchResult: PHFetchResult<PHAsset>?
+    
+    weak var observer: AssetServiceObserver?
+    
+    override init() {
+        super.init()
+        
+        PHPhotoLibrary.shared().register(self)
+    }
+    
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
+    }
+    
     func fetchImageAssets(with options: PHFetchOptions?, completion: (([PHAsset]) -> Void)?) {
         let result = PHAsset.fetchAssets(with: .image, options: options)
         var assets = [PHAsset]()
@@ -18,6 +32,7 @@ class AssetServiceProvider: AssetService {
             assets.append(asset)
         })
         completion?(assets)
+        fetchResult = result
     }
     
     func fetchImage(for data: AssetRequestData, completion: ((UIImage?) -> Void)?) {
@@ -30,5 +45,24 @@ class AssetServiceProvider: AssetService {
         manager.requestImage(for: asset, targetSize: data.size, contentMode: data.mode, options: data.options, resultHandler: { image, _ in
             completion?(image)
         })
+    }
+}
+
+extension AssetServiceProvider: PHPhotoLibraryChangeObserver {
+    
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        DispatchQueue.main.sync {
+            guard let result = fetchResult,
+                let details = changeInstance.changeDetails(for: result) else {
+                return
+            }
+            
+            fetchResult = details.fetchResultAfterChanges
+            var assets = [PHAsset]()
+            fetchResult!.enumerateObjects({ (asset, _, _) in
+                assets.append(asset)
+            })
+            observer?.assetServiceDidChange(assets: assets)
+        }
     }
 }
