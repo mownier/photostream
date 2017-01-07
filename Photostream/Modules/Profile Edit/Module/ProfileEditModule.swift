4dbc16b80ed87c7235cfddb2ebe7016d03271cbf
Mounir Ybanez <rinuom91@gmail.com>
2017-01-07 13:59:26 +0800

    Added profile edit module builder.

--- Photostream/Modules/Profile Edit/Module/ProfileEditModule.swift
+++ Photostream/Modules/Profile Edit/Module/ProfileEditModule.swift
@@ -18,3 +18,50 @@ protocol ProfileEditModuleInterface: BaseModuleInterface {
     func set(firstName: String)
     func set(lastName: String)
 }
+
+protocol ProfileEditBuilder: BaseModuleBuilder {
+    
+    func build(root: RootWireframe?, data: ProfileEditData)
+}
+
+class ProfileEditModule: BaseModule, BaseModuleInteractable {
+    
+    typealias ModuleView = ProfileEditScene
+    typealias ModuleInteractor = ProfileEditInteractor
+    typealias ModulePresenter = ProfileEditPresenter
+    typealias ModuleWireframe = ProfileEditWireframe
+    
+    var view: ModuleView!
+    var interactor: ModuleInteractor!
+    var presenter: ModulePresenter!
+    var wireframe: ModuleWireframe!
+    
+    required init(view: ModuleView) {
+        self.view = view
+    }
+}
+
+extension ProfileEditModule: ProfileEditBuilder {
+    
+    func build(root: RootWireframe?) {
+        let auth = AuthSession()
+        let userService = UserServiceProvider(session: auth)
+        let fileService = FileServiceProvider(session: auth)
+        
+        interactor = ProfileEditInteractor(userService: userService, fileService: fileService)
+        presenter = ProfileEditPresenter()
+        wireframe = ProfileEditWireframe(root: root)
+        
+        view.presenter = presenter
+        interactor.output = presenter
+        
+        presenter.view = view
+        presenter.interactor = interactor
+        presenter.wireframe = wireframe
+    }
+    
+    func build(root: RootWireframe?, data: ProfileEditData) {
+        build(root: root)
+        presenter.updateData = data
+    }
+}
