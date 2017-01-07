e11e8be4b872ec076b98b6d938f28cd12a973718
Mounir Ybanez <rinuom91@gmail.com>
2017-01-07 13:15:50 +0800

    Added files.

---
+++ Photostream/Modules/Profile Edit/Interactor/ProfileEditData.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileEditData.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 07/01/2017.
+//  Copyright © 2017 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEditData: AnyObject {
+
+}
---
+++ Photostream/Modules/Profile Edit/View/ProfileEditScene.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileEditScene.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 07/01/2017.
+//  Copyright © 2017 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEditScene: AnyObject {
+
+}
