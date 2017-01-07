b822cbf4202ac968cf74d845b32afb64324ded22
Mounir Ybanez <rinuom91@gmail.com>
2017-01-07 13:16:15 +0800

    Added profile edit data.

--- Photostream/Modules/Profile Edit/Interactor/ProfileEditData.swift
+++ Photostream/Modules/Profile Edit/Interactor/ProfileEditData.swift
@@ -6,8 +6,20 @@
 //  Copyright © 2017 Mounir Ybanez. All rights reserved.
 //
 
-import UIKit
+protocol ProfileEditData {
 
-class ProfileEditData: AnyObject {
+    var username: String { set get }
+    var bio: String { set get }
+    var avatarUrl: String { set get }
+    var firstName: String { set get }
+    var lastName: String { set get }
+}
 
+struct ProfileEditDataItem: ProfileEditData {
+    
+    var username: String = ""
+    var bio: String = ""
+    var avatarUrl: String = ""
+    var firstName: String = ""
+    var lastName: String = ""
 }
