42844042157adc2cc313d88b7a46ffd5b150cae4
Mounir Ybanez <rinuom91@gmail.com>
2017-01-07 15:02:23 +0800

    dded module extension.

---
+++ Photostream/Module Extensions/ProfileEditModuleExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileEditModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 07/01/2017.
+//  Copyright © 2017 Mounir Ybanez. All rights reserved.
+//
+
+extension ProfileEditModule {
+    
+    convenience init() {
+        self.init(view: ProfileEditViewController())
+    }
+}
