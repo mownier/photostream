3e0ab3c042155b826835a3d85daffb28c7adaf51
Mounir Ybanez <rinuom91@gmail.com>
2017-01-07 13:49:06 +0800

    Added tracking of upload progress.

--- Photostream/Modules/Profile Edit/View/ProfileEditScene.swift
+++ Photostream/Modules/Profile Edit/View/ProfileEditScene.swift
@@ -6,10 +6,13 @@
 //  Copyright © 2017 Mounir Ybanez. All rights reserved.
 //
 
+import Foundation
+
 protocol ProfileEditScene: BaseModuleView {
 
     var presenter: ProfileEditModuleInterface! { set get }
     
     func didUpdate(with error: String?)
     func didUpload(with error: String?)
+    func didUpload(with progress: Progress)
 }
