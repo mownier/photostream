6b7bea55d02fe77e39724d8241b0a1918046620f
Mounir Ybanez <rinuom91@gmail.com>
2017-01-16 13:20:58 +0800

    Added module extension.

---
+++ Photostream/Module Extensions/MultilineEditorModuleExtension.swift
@@ -0,0 +1,14 @@
+//
+//  MultilineEditorModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 16/01/2017.
+//  Copyright © 2017 Mounir Ybanez. All rights reserved.
+//
+
+extension MultilineEditorModule {
+    
+    convenience init() {
+        self.init(view: MultilineEditorViewController())
+    }
+}
