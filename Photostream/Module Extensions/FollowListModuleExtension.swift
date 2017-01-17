2655a5275d377b5e0f637f8a35a478f83d9600f6
Mounir Ybanez <rinuom91@gmail.com>
2017-01-17 15:36:05 +0800

    Added follow list module extension.

---
+++ Photostream/Module Extensions/FollowListModuleExtension.swift
@@ -0,0 +1,13 @@
+//
+//  FollowListModuleExtension.swift
+//  Photostream
+//
+//  Created by Mounir Ybanez on 17/01/2017.
+//  Copyright © 2017 Mounir Ybanez. All rights reserved.
+//
+
+import UIKit
+
+class FollowListModuleExtension: AnyObject {
+
+}
