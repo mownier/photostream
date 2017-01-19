fb51855fecceab859b6c9d5bdeb32e39a9441292
Mounir Ybanez <rinuom91@gmail.com>
2017-01-19 11:25:22 +0800

    Added extension for presenting single post module.

--- Photostream/Module Extensions/UserActivityModuleExtension.swift
+++ Photostream/Module Extensions/UserActivityModuleExtension.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Mounir Ybanez. All rights reserved.
 //
 
+import UIKit
+
 extension UserActivityModule {
     
     convenience init() {
@@ -38,6 +40,50 @@ extension UserActivityFollowDataItem: ActivityTableCellFollowItem {
     
     var content: String {
         return "\(displayName) started following you."
+    }
+}
+
+extension UserActivityModuleInterface {
+    
+    func presentSinglePost(for index: Int) {
+        guard let presenter = self as? UserActivityPresenter,
+            let activity = activity(at: index) else {
+            return
+        }
+        
+        var postId: String?
+        
+        if let data = activity as? UserActivityLikeData {
+            postId = data.postId
+            
+        } else if let data = activity as? UserActivityCommentData {
+            postId = data.postId
+            
+        } else if let data = activity as? UserActivityPostData {
+            postId = data.postId
+        }
+        
+        let parent = presenter.view.controller
+        presenter.wireframe.showSinglePost(parent: parent, postId: postId)
+    }
+}
+
+extension UserActivityWireframeInterface {
+    
+    func showSinglePost(parent: UIViewController?, postId: String?) {
+        guard postId != nil else {
+            return
+        }
+        
+        let module = SinglePostModule()
+        module.build(root: root, postId: postId!)
+        
+        var property = WireframeEntryProperty()
+        property.controller = module.view.controller
+        property.parent = parent
+        
+        module.wireframe.style = .push
+        module.wireframe.enter(with: property)
     }
 }
 
