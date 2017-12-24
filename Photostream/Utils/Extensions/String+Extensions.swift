396971dc7ac04bc0bf40ed2e2c4de333f11eebf4
Vanson <Vanson@live.cn>
2017-12-24 23:21:19 +0800

    Fix compile warning in Xcode9.2

--- Photostream/Utils/Extensions/String+Extensions.swift
+++ Photostream/Utils/Extensions/String+Extensions.swift
@@ -10,18 +10,38 @@ import Foundation
 
 extension String {
 
-    subscript (i: Int) -> Character {
-        return self[self.characters.index(self.startIndex, offsetBy: i)]
+    var length: Int {
+        return self.count
     }
-
+    
     subscript (i: Int) -> String {
-        return String(self[i] as Character)
+        return self[i ..< i + 1]
     }
-
+    
+    func substring(fromIndex: Int) -> String {
+        return self[min(fromIndex, length) ..< length]
+    }
+    
+    func substring(toIndex: Int) -> String {
+        return self[0 ..< max(0, toIndex)]
+    }
+    
     subscript (r: Range<Int>) -> String {
-        let start = characters.index(startIndex, offsetBy: r.lowerBound)
-        let end = index(start, offsetBy: r.upperBound - r.lowerBound)
-        return String(self[Range(start ..< end)])
+        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
+                                            upper: min(length, max(0, r.upperBound))))
+        let start = index(startIndex, offsetBy: range.lowerBound)
+        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
+        return String(self[start ..< end])
+    }
+    
+    func reverse() -> String {
+        let count = self.count
+        if count == 0 { return "" }
+        var result = ""
+        for i in 0..<count {
+            result = self[i] + result
+        }
+        return result
     }
 }
 
